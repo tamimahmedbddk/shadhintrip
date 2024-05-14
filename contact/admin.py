5cad27c844e566822d267cc1914bd43f16ace14f
adiai ismat <121921125+adiaismatlamat7594@users.noreply.github.com>
2024-05-14 14:40:09 +0600

    added contact option

---
+++ contact/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import ContactMessage
+
+# Register your models here, if any
+admin.site.register(ContactMessage)
---
+++ contact/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, redirect
+from .forms import ContactForm
+
+def contact_us(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('contact_success')
+    else:
+        form = ContactForm()
+    return render(request, 'contact/contact_us.html', {'form': form})
+
+def contact_success(request):
+    return render(request, 'contact/contact_success.html')
