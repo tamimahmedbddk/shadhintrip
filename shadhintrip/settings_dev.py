b1f529bab7193c0c248aa7d7cb5107d4e48348b4
adiai ismat <121921125+adiaismatlamat7594@users.noreply.github.com>
2024-05-16 15:04:15 +0600

    sdf

---
+++ shadhintrip/settings_dev.py
@@ -0,0 +1,9 @@
+# ==========comamnd to drop migratios files=======
+######## find . -name "__pycache__" -type d -exec rm -r {} +  ##########
+
+
+# find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
+# find . -path "*/migrations/*.pyc" -delete
+# ==========comamnd to drop migratios files=======
+
+# ==========psql comamnd to drop tables=======
