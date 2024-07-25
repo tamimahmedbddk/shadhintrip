d4458c2118a3d38230f5231842e66d09dd3d2e12
adiai ismat <121921125+adiaismatlamat7594@users.noreply.github.com>
2024-07-25 18:05:51 +0600

    sf

--- shadhintrip/settings_dev.py
+++ shadhintrip/settings_dev.py
@@ -1,6 +1,12 @@
 
 from .settings import *
 
+# Development-specific settings
+DEBUG = True
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+SECRET_KEY='25^l5j0a(!o6am^-$6y_tnluht0n-3z=zpemm%=&(=95o%)h6o'
+
+
 #dbbackup on local host
 
 DBBACKUP_STORAGE = 'django.core.files.storage.FileSystemStorage'
