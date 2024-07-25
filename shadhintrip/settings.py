2edf237df28e98eca5a29a9f1bc82541ee09869d
adiai ismat <121921125+adiaismatlamat7594@users.noreply.github.com>
2024-07-25 18:05:51 +0600

    sf

--- shadhintrip/settings.py
+++ shadhintrip/settings.py
@@ -16,14 +16,6 @@ else:
     from .settings_dev import *
 
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j5n)oae-5v!rm(+$vktd2^hy!(9@@zn@#p_!)&7ug$ksjwyk8#'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['*']
-
 # settings.py
 AUTH_USER_MODEL = 'users.CustomUser'
 
@@ -136,7 +128,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = (
    os.path.join(BASE_DIR, 'static'),
 )
--- shadhintrip/settings_prod.py
+++ shadhintrip/settings_prod.py
@@ -1,5 +1,10 @@
 from .settings import *
 
+# Production-specific settings
+DEBUG=False
+ALLOWED_HOSTS = ['markcc.shop','www.markcc.shop','localhost']
+SECRET_KEY='25^l5j0a(!o6am^-$6y_tnluht0n-3z=zpemm%=&(=95o%)h6o'
+
 #dbbackup on production
 DBBACKUP_STORAGE = 'django.core.files.storage.FileSystemStorage'
 DBBACKUP_STORAGE_OPTIONS = {'location': '/home/shadhintrip/backup/'}
