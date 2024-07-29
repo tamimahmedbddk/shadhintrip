444a826e130e9b43fa3ed1ff846928ede51492cc
adiai ismat <121921125+adiaismatlamat7594@users.noreply.github.com>
2024-07-29 06:20:38 +0600

    new requirements.txt file added

--- bookings/views.py
+++ bookings/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from.models import VisaBooking,TourBooking
+from django.core.paginator import Paginator
 
 from django.contrib.auth import login as auth_login
 from django.contrib.auth.models import User
@@ -10,9 +11,22 @@ from django.contrib.auth.models import User
 @login_required(login_url='login')
 def booking_history(request):
     visa_bookings = VisaBooking.objects.all().filter(user=request.user).order_by('-id')
+    visa_bookings_paginator = Paginator(visa_bookings, 10)  # Show 10 visa packages per page
+    page_number = request.GET.get('page')
+    visa_booking_page_obj = visa_bookings_paginator.get_page(page_number)
+
+
     tour_bookings = TourBooking.objects.all().filter(user=request.user).order_by('-id')
+    tour_bookings_paginator = Paginator(tour_bookings, 10)  # Show 10 visa packages per page
+    page_number = request.GET.get('page')
+    tour_booking_page_obj = tour_bookings_paginator.get_page(page_number)
+
     context = {
-        'bookings': visa_bookings,
-        'tour_bookings':tour_bookings,
+        'visa_bookings': visa_booking_page_obj,  # Pass the paginated bookings
+        'visa_booking_page_obj': visa_booking_page_obj,  # Pass the page object for pagination controls
+
+
+        'tour_bookings':tour_booking_page_obj,
+        'tour_booking_page_obj': tour_booking_page_obj,
     }
     return render(request, 'bookings/booking-history.html', context)
